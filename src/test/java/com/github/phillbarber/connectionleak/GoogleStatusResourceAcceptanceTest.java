3594e38119c40e5bc998ac66c1e3227f44cf5fb9
phillbarber <philliprbarber@googlemail.com>
2015-01-16 21:10:41 +0000

    Added test with retry of 2

--- src/test/java/com/github/phillbarber/connectionleak/GoogleStatusResourceAcceptanceTest.java
+++ src/test/java/com/github/phillbarber/connectionleak/GoogleStatusResourceAcceptanceTest.java
@@ -4,10 +4,9 @@ package com.github.phillbarber.connectionleak;
 import com.google.common.io.Resources;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
-import io.dropwizard.client.JerseyClientBuilder;
-import io.dropwizard.setup.Environment;
 import io.dropwizard.testing.junit.DropwizardAppRule;
 import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.File;
@@ -19,14 +18,13 @@ public class GoogleStatusResourceAcceptanceTest {
 
     @ClassRule
     public static final DropwizardAppRule appRule = new DropwizardAppRule<AppConfig>(ConnectionLeakApp.class, getAbsolutePath());
-    public static final int ExXPECTED_STATUS_CODE_OF_PING_GOOGLE_TEST = 200;
-    private Client client = new JerseyClientBuilder(appRule.getEnvironment()).build("");
 
-
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
 
     private static String getAbsolutePath()  {
         try {
-            return new File(Resources.getResource("config.yml").toURI()).getAbsolutePath();
+            return new File(Resources.getResource("config-unit-test.yml").toURI()).getAbsolutePath();
         } catch (URISyntaxException e) {
             throw new RuntimeException(e);
         }
@@ -34,14 +32,11 @@ public class GoogleStatusResourceAcceptanceTest {
 
 
     @Test
+    @Repeat(times=2)
     public void testPingGoogle(){
-
-        ClientResponse clientResponse = client.resource("http://localhost:" + appRule.getLocalPort() + "/google-status").get(ClientResponse.class);
-        assertEquals(ExXPECTED_STATUS_CODE_OF_PING_GOOGLE_TEST, clientResponse.getStatus());
+        ClientResponse clientResponse = new Client().resource("http://localhost:" + appRule.getLocalPort()).path("/google-status").get(ClientResponse.class);
         assertEquals("Google is returning a 200 response", clientResponse.getEntity(String.class));
         clientResponse.close();
     }
-
-
 
 }
